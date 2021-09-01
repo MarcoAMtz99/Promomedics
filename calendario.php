24cc0e209c02e561595ec4daee2b21a3f2f96ac1
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-09-01 11:30:53 -0500

    Inicio de calendario

--- calendario.php
+++ calendario.php
@@ -1,16 +1,6 @@
 <?php 
   $titulo = "Calendario";
   include 'header.php'; 
-
-  $aperm = $_SESSION['perm'];
-  if(!array_key_exists(MOD_MEDICOS, $aperm)){
-    include '403.php';
-    exit(0);
-  }else{
-    $perm = $aperm[MOD_MEDICOS];
-    $perm = $perm['action'];
-  }
-
 ?>
 
     <!-- iCheck -->
