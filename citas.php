bc892f65f5e1a27415a0a545b9573cdcb498bad0
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-09-07 07:16:19 -0500

    Prueba en calendario

--- citas.php
+++ citas.php
@@ -1,6 +1,6 @@
 <?php 
 
-/* 	header('Content-Type: application/json'); */
+	header('Content-Type: application/json');
 /* 	$conexion = mysqli_connect("localhost","root" ,"!_WOXa9ZxWfP","bywsicom_promo"); */
 	/* $conn = mysql_pconnect('localhost','bywsicom_promo','!_WOXa9ZxWfP');
 	mysql_select_db('bywsicom_promo'); */
@@ -57,34 +57,8 @@
 			);
 			array_push($arrItems,$array);
 	}
-	$array2 = array('AGENDA' => $arrItems);
+		$array2 = array('AGENDA' => $arrItems);
 		return json_encode($array2);
 
-
-
-	/* VERSIION 7.3 */
-/* 	header('Content-Type: application/json');
-	$conexion = mysqli_connect("localhost","root" ,"","promo");
-	$pdo = new PDO("mysql:dbname=promo; host=localhost","root","");
-	$sql = $pdo->prepare("select id,
-							 (select nombre from medico where ID = id_medico) as nombre,
-							(select paterno from medico where ID = id_medico) as paterno,
-							(select materno from medico where ID = id_medico) as materno,
-							paciente,
-							fecha_consulta as start,
-							hora_consulta,
-							aseguradora,
-							telefono1,
-							telefono2,
-							telefono3,
-							costoConsulta,
-							recado,
-							edad,
-							comoSeEntero
-							 from agenda;");
-	$sql->execute();
-
-	$resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
-	echo json_encode($resultado); */
 
  ?>
