af2a83fd3497d7e7340db1aacbd846b787e06887
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-09-07 06:09:07 -0500

    Prueba en calendario

--- calendario.php
+++ calendario.php
@@ -19,7 +19,7 @@
 		<script src="js/moment.min.js"></script>
 		<script src="js/fullcalendar.min.js"></script>
 		<script src="js/es.js"></script>
-		<!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script> -->
+		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
  		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
 
 
