ad8350917a96e3dbcb221d64d9335a4d18c835ef
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-09-07 08:01:02 -0500

    Prueba en calendario

--- calendario.php
+++ calendario.php
@@ -14,7 +14,7 @@
 
 		 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-  <img class="logo-sm" src="/images/logo_small.png" alt="AGENDA">
+  <img class="" style="height: 40px;" src="/images/logo_small.png" alt="AGENDA">
     <a class="navbar-brand" href="https://promomedics.byw-si.com.mx/">HOME</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
