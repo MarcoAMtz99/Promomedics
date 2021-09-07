41f9a1f1d4197ee7f9e64f6b77896b7a5d8bfca2
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-09-07 06:09:42 -0500

    Prueba en calendario

--- calendario.php
+++ calendario.php
@@ -13,13 +13,13 @@
 
 ?>
 
-	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"> -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/fullcalendar.min.css">
 		<script src="js/jquery.min.js"></script>
 		<script src="js/moment.min.js"></script>
 		<script src="js/fullcalendar.min.js"></script>
 		<script src="js/es.js"></script>
-		<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+		<!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script> -->
  		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
 
 
