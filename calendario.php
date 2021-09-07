b94e84879b74b7218228cb1dc68329858ffce93f
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-09-07 08:13:28 -0500

    Prueba en calendario

--- calendario.php
+++ calendario.php
@@ -12,7 +12,7 @@
 		<!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script> -->
  		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
 
-		 <nav class="navbar navbar-expand-lg navbar-light bg-light">
+		 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
   
     <a class="fa fa-home" href="https://promomedics.byw-si.com.mx/">INICIO</a>
