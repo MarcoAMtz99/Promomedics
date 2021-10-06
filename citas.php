94899bd3b7034e8452d7f59c454e7bcaa32d8cd3
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-10-06 05:36:13 -0500

    Cambios para configurar el servidor

--- citas.php
+++ citas.php
@@ -1,6 +1,6 @@
 <?php 
 
-	header('Content-Type: application/json');
+	// header('Content-Type: application/json');
 	$conexion = mysqli_connect('localhost','bywsicom_promo','!_WOXa9ZxWfP','bywsicom_promo');
 	// $conexion = mysqli_connect("localhost","root" ,"","promo");
 	$pdo = new PDO("mysql:dbname=bywsicom_promo; host=localhost","bywsicom_promo","!_WOXa9ZxWfP");
