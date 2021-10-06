3320a2c46e33af7b4c2044e1896150470addc091
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-10-06 02:45:05 -0500

    Cambios para configurar el servidor

---
+++ agendar.php
@@ -0,0 +1,26 @@
+<?php 
+
+	header('Content-Type: application/json');
+	//desarrollo 
+	// $conexion = mysqli_connect("localhost","root" ,"","promo");
+	// $pdo = new PDO("mysql:dbname=promo; host=localhost","root","");
+	//PRODUCCION
+	$conexion = mysqli_connect("localhost","bywsicom_promo" ,"!_WOXa9ZxWfP","bywsicom_promo");
+		// $conn = mysqli_connect('localhost','bywsicom_promo','!_WOXa9ZxWfP','bywsicom_promo');
+	$pdo = new PDO("mysql:dbname=bywsicom_promo; host=localhost","bywsicom_promo","!_WOXa9ZxWfP");
+
+	$paciente = $_POST["paciente"];
+	$fecha_consulta = $_POST["fecha"];
+	$costoConsulta = $_POST["costoConsulta"];
+
+
+
+	$sql = $pdo->prepare("INSERT INTO `agenda`(`id_medico`, `status`, `fechaCreacion`, `fechaActualizacion`, `usuarioCreacionId`, `usuarioActualizacionId`, `id_consultorio`, `paciente`, `fecha_consulta`, `hora_consulta`, `aseguradora`, `mail`, `telefono1`, `telefono2`, `telefono3`, `consultaPrimeraVez`, `consultaSubsecuente`, `consultaPreferencial1`, `consultaPreferencial2`, `consultaRevision`, `consultaEstudios`, `consultaUrgencia`, `costoConsulta`, `recado`, `comoSeEntero`, `edad`, `tutor`) VALUES (10,1,'2021-09-07','2021-09-07',1,1,14,'$paciente','$fecha_consulta','12:00:00','GNP','prueba@gmail.com','1234567890','1234567890','1234567890',0,0,0,0,0,0,0,'$costoConsulta','prueba','internet',25,null)");
+
+	$sql->execute();
+
+	// $resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
+	header("Location: calendario.php");
+	// echo json_encode($sql->execute());
+
+ ?>
