d9f5b5b993c0d1357323a16d724b1708b2bc610a
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-10-06 06:37:10 -0500

    Cambios para configurar el servidor

--- modulos.php
+++ modulos.php
@@ -98,7 +98,7 @@
           </div>
 
           <?php if($perm == 'EDIT') : ?>
-          <div id="frm-item" class="modal" tabindex="-1" role="dialog" aria-hidden="true">
+          <div id="frm-item" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
             <div class="modal-dialog">
               <div class="modal-content">
                 <div class="modal-header">
@@ -188,6 +188,30 @@
           
         </div>
         <!-- /page content -->
+        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalLong">
+  Launch demo modal
+</button>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModalLong" tabindex="-1" role="dialog" aria-labelledby="exampleModalLongTitle" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        ...
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 
         <?php include 'footer.php'; ?>
 
