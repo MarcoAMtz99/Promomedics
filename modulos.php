8ce8a8d5071bb7805cf31b74b9d2a128cb3f27b7
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-10-06 06:32:17 -0500

    Cambios para configurar el servidor

--- modulos.php
+++ modulos.php
@@ -98,7 +98,7 @@
           </div>
 
           <?php if($perm == 'EDIT') : ?>
-          <div id="frm-item" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+          <div id="frm-item" class="modal" tabindex="-1" role="dialog" aria-hidden="true">
             <div class="modal-dialog">
               <div class="modal-content">
                 <div class="modal-header">
