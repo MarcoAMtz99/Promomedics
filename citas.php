58f303302a8e84d62f20d742353120fdb26de111
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-10-06 03:32:14 -0500

    Cambios para configurar el servidor

--- citas.php
+++ citas.php
@@ -7,20 +7,14 @@
 	// mysql_select_db('bywsicom_promo'); 
 	/* include 'conex.php'; */
 	$conexion = mysqli_connect("localhost","bywsicom_promo" ,"!_WOXa9ZxWfP","bywsicom_promo");
-	// $enlace =  mysqli_connect('localhost', 'bywsicom_promo', '!_WOXa9ZxWfP');
-			/* if (!$enlace) {
-				die('No pudo conectarse: ' . mysql_error());
-			}
-			echo 'Conectado satisfactoriamente'; */
-			//PRODUCTIVO
-			// mysqli_select_db($conexion,'bywsicom_promo');
-			// DESARROLLO
-			// mysqli_select_db($conexion,'promo');
-	$sql = "select id,
-							(select nombre from medico where ID = id_medico) as nombre,
+	
+	// $conexion = mysqli_connect("localhost","root" ,"","promo");
+	$pdo = new PDO("mysql:dbname=bywsicom_promo; host=localhost","bywsicom_promo","!_WOXa9ZxWfP");
+	$sql = $pdo->prepare("select id,
+							 (select nombre from medico where ID = id_medico) as nombre,
 							(select paterno from medico where ID = id_medico) as paterno,
 							(select materno from medico where ID = id_medico) as materno,
-							paciente,
+							paciente as title,
 							fecha_consulta as start,
 							hora_consulta,
 							aseguradora,
@@ -31,40 +25,12 @@
 							recado,
 							edad,
 							comoSeEntero
-							 from agenda;";
-	$SQL2 ='select * from agenda';
 
-	$res = mysqli_query($conexion,$SQL2);
-		
-	$arrItems = array();
-	while ($fila = mysqli_fetch_assoc($res)) {
-	
-	// 	/* echo $fila['paciente'] ; */
-	
-		$array = array(
-			'id' => $fila['id'],
-			'nombre' => $fila['nombre'],
-			'paterno' => $fila['paterno'],
-			'materno' => $fila['materno'],
-			'paciente' => $fila['paciente'],
-			'start' => $fila['start'],
-			'hora_consulta' => $fila['hora_consulta'],
-			'telefono1' => $fila['telefono1'],
-			'telefono2' => $fila['telefono2'],
-			'telefono3' => $fila['telefono3'],
-			'costoConsulta' => $fila['costoConsulta'],
-			'aseguradora' => $fila['aseguradora'],
-			'recado' => $fila['recado'],
-			'edad' => $fila['edad'],
-			'comoSeEntero' => $fila['comoSeEntero']
-			);
-			array_push($arrItems,$array);
-	}
-		$auxiliar = array('AGENDA' => $arrItems);
-		$resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
-		echo json_encode($resultado);
-		echo json_encode($auxiliar);
-	/* 	break; */
+							 from agenda;");
+	$sql->execute();
+
+	$resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
+	echo json_encode($resultado);
 
 
  ?>
