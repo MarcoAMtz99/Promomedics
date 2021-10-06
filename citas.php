9f461ae69f8a738cad4c1a90aaabe8227c9ce356
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-10-06 06:02:51 -0500

    Cambios para configurar el servidor

--- citas.php
+++ citas.php
@@ -27,19 +27,43 @@
 
 							 from agenda;");
 	$sql->execute();
-	$array= array();
-	$resultado2 = mysqli_query($conexion, $consulta);
-	while ($row = mysqli_fetch_assoc($resultado)) {
-		echo $row;
-       $dato = $row['nombre'];
-       array_push($array, $dato);
 
+	//create connection and select DB
+    $db = new mysqli("localhost", "bywsicom_promo", "!_WOXa9ZxWfP", "bywsicom_promo");
+    if($db->connect_error){
+        die("Unable to connect database: " . $db->connect_error);
     }
+    
+    //get user data from the database
+    $query = $db->query("select id,
+							 (select nombre from medico where ID = id_medico) as nombre,
+							(select paterno from medico where ID = id_medico) as paterno,
+							(select materno from medico where ID = id_medico) as materno,
+							paciente as title,
+							fecha_consulta as start,
+							hora_consulta,
+							aseguradora,
+							telefono1,
+							telefono2,
+							telefono3,
+							costoConsulta,
+							recado,
+							edad,
+							comoSeEntero
+
+							 from agenda;");
+    
+    if($query->num_rows > 0){
+        $userData = $query->fetch_assoc();
+        $data['status'] = 'ok';
+        $data['result'] = $userData;
+    }else{
+        $data['status'] = 'err';
+        $data['result'] = '';
+    }
+
 	$resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
-	while (true) {
-		// echo json_encode($resultado);
-		break;
-	}
+	 echo json_encode($data);
 	echo "403.pHP AQUI SE VE SI SSALE EL ECHOO PRIMERO";
 	echo "<br>";
 	echo "<br>";
