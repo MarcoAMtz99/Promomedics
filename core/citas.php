56d7b06a12de3fb1a90703bbae1ef38ce1335a4d
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-10-06 05:34:35 -0500

    Cambios para configurar el servidor

---
+++ core/citas.php
@@ -0,0 +1,35 @@
+<?php 
+
+	header('Content-Type: application/json');
+	$conexion = mysqli_connect('localhost','bywsicom_promo','!_WOXa9ZxWfP','bywsicom_promo');
+	// $conexion = mysqli_connect("localhost","root" ,"","promo");
+	$pdo = new PDO("mysql:dbname=bywsicom_promo; host=localhost","bywsicom_promo","!_WOXa9ZxWfP");
+	$sql = $pdo->prepare("select id,
+							 (select nombre from medico where ID = id_medico) as nombre,
+							(select paterno from medico where ID = id_medico) as paterno,
+							(select materno from medico where ID = id_medico) as materno,
+							paciente as title,
+							fecha_consulta as start,
+							hora_consulta,
+							aseguradora,
+							telefono1,
+							telefono2,
+							telefono3,
+							costoConsulta,
+							recado,
+							edad,
+							comoSeEntero
+
+							 from agenda;");
+	$sql->execute();
+
+	$resultado = $sql->fetchAll(PDO::FETCH_ASSOC);
+	while (true) {
+		echo json_encode($resultado);
+		break;
+	}
+	
+	// print_r($resultado);
+	// header("Location: calendario.php");
+
+ ?>
