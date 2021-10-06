d2e973adfde3a0cae64f9a3384ec43ea9849b379
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-10-06 02:53:22 -0500

    Cambios para configurar el servidor

--- calendario.php
+++ calendario.php
@@ -193,7 +193,7 @@
 								      </div>
 								      <div class="modal-footer">
 								       
-								         <button type="button" class="btn btn-primary">AGENDAR</button>
+								         <button type="submit" class="btn btn-primary">AGENDAR</button>
 								
 								         <button type="button" class="btn btn-secondary" data-dismiss="modal">CANCELAR</button>
 								      </div>
