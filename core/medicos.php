24ae81bb0b49becf03020c89e042fddc291cd055
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-10-07 03:29:51 -0500

    Cambios en core/medicos botones

--- core/medicos.php
+++ core/medicos.php
@@ -82,7 +82,7 @@ switch ($action) {
 			$item['rea'] = '';
 			$item['neg'] = '';
 			if($item['fk_perfil'] == 3 && $edita && $item['status'] == 3){
-				$item['act'] = '<button class="btn btn-success btn-xs btn-act" data-toggle="tooltip" title="Activar"><i class="fa fa-check fa-fw"></i></button>';	
+				$item['act'] = '<button class="btn btn-success add-link" data-toggle="tooltip" title="Activar"><i class="fa fa-check fa-fw"></i></button>';	
 				$item['rea'] = '<button class="btn btn-info btn-xs btn-rea" data-toggle="tooltip" title="Reactivar"><i class="fa fa-refresh fa-fw"></i></button>';
 				$item['neg'] = '<button class="btn btn-danger btn-xs btn-neg" data-toggle="tooltip" title="Negar"><i class="fa fa-ban fa-fw"></i></button>';
 			}else if($item['fk_perfil'] == 4 && $edita){
