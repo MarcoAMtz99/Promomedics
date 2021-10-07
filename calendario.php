8c0ec037ca4394eb762a323732a1aa0f73780e84
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-10-07 06:44:14 -0500

    Cambios en core/medicos botones

--- calendario.php
+++ calendario.php
@@ -52,7 +52,7 @@
 		
 		$(document).ready(function(){
 			 $.ajax({
-			      url: "http://api.promo.byw-si.com.mx/agenda",
+			      url: "https://api.promo.byw-si.com.mx/agenda",
 			      method: "GET",
 			      dataType: "json"
 			    }).done(function( data ) {
