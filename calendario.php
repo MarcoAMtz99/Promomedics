777d15ff13c140b05e9c47cc0d86f6dc8d5f0434
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-10-07 06:32:50 -0500

    Cambios en core/medicos botones

--- calendario.php
+++ calendario.php
@@ -7,6 +7,7 @@
 		<!-- <script src="js/es.js"></script> -->
 		<!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script> -->
  		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"></script>
+ 		<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
 
 	<!-- 	 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <div class="container-fluid">
