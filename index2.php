9f81989523dac15d3568f509046bc6de8782a6d5
Marco Antonio Martinez Martinez <38748617+MarcoAMtz99@users.noreply.github.com>
2021-11-03 02:17:56 -0500

    Prueba en nueva plantilla

--- index2.php
+++ index2.php
@@ -155,32 +155,32 @@
                  if (isset( $permArray)) {
                       # code...
                     
-                    // foreach ($permArray as $perm) {
-                    //   if(isset($perm['children'])){
-                    //     echo '<li  id="mnu-'.$perm['abrev'].'"><a><i class="fa fa-'.$perm['icono'].'"></i> '.$perm['nombre'].' <span class="fa fa-chevron-down"></span></a>';
-                    //     echo '<ul class="nav child_menu">';
+                    foreach ($permArray as $perm) {
+                      // if(isset($perm['children'])){
+                      //   echo '<li  id="mnu-'.$perm['abrev'].'"><a><i class="fa fa-'.$perm['icono'].'"></i> '.$perm['nombre'].' <span class="fa fa-chevron-down"></span></a>';
+                      //   echo '<ul class="nav child_menu">';
 
-                    //     foreach ($perm['children'] as $mnu) {
-                    //       echo '<li  id="mnu-'.$mnu['abrev'].'"><a href="'.URL_ROOT.$mnu['url'].'">'.$mnu['nombre'].'</a></li>';
-                    //     }
+                      //   foreach ($perm['children'] as $mnu) {
+                      //     echo '<li  id="mnu-'.$mnu['abrev'].'"><a href="'.URL_ROOT.$mnu['url'].'">'.$mnu['nombre'].'</a></li>';
+                      //   }
 
-                    //     echo "</ul>";
-                    //     echo "</li>";
-                    //   }else{
-                    //     if($perm['url'] == '/medico' && ($usertype == 3 || $usertype == 4)) $perm['url'] .= '/'.$_SESSION['medico'];
+                      //   echo "</ul>";
+                      //   echo "</li>";
+                      // }else{
+                      //   if($perm['url'] == '/medico' && ($usertype == 3 || $usertype == 4)) $perm['url'] .= '/'.$_SESSION['medico'];
 
-                    //     if($perm['url'] == '/gpomedico'){
-                    //       if($perm['url'] == '/gpomedico' && ($usertype == 3 || $usertype == 4) && $_SESSION['grupo'] != 0){
-                    //         $perm['url'] .= '/'.$_SESSION['grupo'];
-                    //         echo '<li id="mnu-'.$perm['abrev'].'"><a href="'.URL_ROOT.$perm['url'].'"><i class="fa fa-'.$perm['icono'].'"></i> '.$perm['nombre'].'</a></li>';
-                    //       }
-                    //     }else{
-                    //       echo '<li  id="mnu-'.$perm['abrev'].'"><a href="'.URL_ROOT.$perm['url'].'"><i class="fa fa-'.$perm['icono'].'"></i> '.$perm['nombre'].'</a></li>';
-                    //     }
-                    //   }
-                     
-                    // }
-                     echo '<li class="list-group-item"><a href="'.URL_ROOT.$perm['url'].'" >'.$perm['nombre'].'<i class="glyphicon glyphicon-lock"></i>Login</a></li>';
+                      //   if($perm['url'] == '/gpomedico'){
+                      //     if($perm['url'] == '/gpomedico' && ($usertype == 3 || $usertype == 4) && $_SESSION['grupo'] != 0){
+                      //       $perm['url'] .= '/'.$_SESSION['grupo'];
+                      //       echo '<li id="mnu-'.$perm['abrev'].'"><a href="'.URL_ROOT.$perm['url'].'"><i class="fa fa-'.$perm['icono'].'"></i> '.$perm['nombre'].'</a></li>';
+                      //     }
+                      //   }else{
+                      //     echo '<li  id="mnu-'.$perm['abrev'].'"><a href="'.URL_ROOT.$perm['url'].'"><i class="fa fa-'.$perm['icono'].'"></i> '.$perm['nombre'].'</a></li>';
+                      //   }
+                      // }
+                      echo '<li class="list-group-item"><a href="'.URL_ROOT.$perm['url'].'" >'.$perm['nombre'].'<i class="glyphicon glyphicon-lock"></i>Login</a></li>';
+                    }
+                    
 
                   } //Fin del if para validar isset 
                     echo' <li id="mnu-cal"><a href="'.URL_ROOT.'/calendario.php"><i class="glyphicon glyphicon-calendar"></i> Agenda</a></li>';
