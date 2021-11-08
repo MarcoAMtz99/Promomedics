da5e9e5bf481d9351948ac76dc898f42b2a1d5ef
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-11-08 00:18:29 -0600

    Cambios en calendario 3

--- index2.php
+++ index2.php
@@ -73,9 +73,9 @@
           <!-- Collect the nav links, forms, and other content for toggling -->
           <div id="bs-content-row-navbar-collapse-5" class="collapse navbar-collapse">
             <ul class="nav navbar-nav navbar-right">
-              <!-- <li class="active"><a href="getting-started.html">Getting Started</a></li>
-              <li class="active"><a href="index.html">Documentation</a></li> -->
-              <!-- <li class="disabled"><a href="#">Link</a></li> -->
+              <li class="active"><a href="getting-started.html"></a></li>
+              <li class="active"><a href="index.html"></a></li>
+              <li class="disabled"><a href="#"></a></li>
               <li class="dropdown">
                 <a data-toggle="dropdown" class="dropdown-toggle" href="#"><?php if(isset($username)){echo $username;}?> <b class="caret"></b></a>
                 <ul role="menu" class="dropdown-menu">
