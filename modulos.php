9cb5026cd77a9936446faa109d7ed7fd92cbcd64
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-11-08 02:40:57 -0600

    Cambios en calendario 3

--- modulos.php
+++ modulos.php
@@ -122,7 +122,7 @@
             <div class="col-md-12 col-sm-12 col-xs-12">
                   <div class="x_panel">
                     <div class="x_title">
-                      <h2>Módulos <small>Menús de primer nivel</small></h2>
+                                             
                       <ul class="nav navbar-right panel_toolbox">
                         <?php if($perm == 'EDIT') : ?>
                         <li>
