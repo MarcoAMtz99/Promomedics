34069378f68b4c5a9c6415ed37085500c7fcc924
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-11-08 00:54:43 -0600

    Cambios en calendario 3

--- index2.php
+++ index2.php
@@ -65,9 +65,9 @@
 
           <div class="navbar-header">
 
-            <button data-target="#bs-content-row-navbar-collapse-5" data-toggle="collapse" class="navbar-toggler" type="button">
+         <!--    <button data-target="#bs-content-row-navbar-collapse-5" data-toggle="collapse" class="navbar-toggler" type="button">
               <span class="navbar-toggler-icon"></span>
-            </button>
+            </button> -->
            
           </div>
           <!--  <a href="#" class="navbar-brand">PROMOMEDICS</a> -->
@@ -102,65 +102,8 @@
 
         </div><!-- /.container-fluid -->
       </nav>
-       <nav role="navigation" class="navbar navbar-custom">
-        <div class="container-fluid">
-          <!-- Brand and toggle get grouped for better mobile display -->
-          <div class="navbar-header">
-            <button data-target="#bs-content-row-navbar-collapse-5" data-toggle="collapse" class="navbar-toggle" type="button">
-              <span class="sr-only">Toggle navigation</span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-              <span class="icon-bar"></span>
-            </button>
-            <a href="#" class="navbar-brand">Bootflat-Admin</a>
-          </div>
-
-          <!-- Collect the nav links, forms, and other content for toggling -->
-          <div id="bs-content-row-navbar-collapse-5" class="collapse navbar-collapse">
-            <ul class="nav navbar-nav navbar-right">
-              <li class="active"><a href="getting-started.html">Getting Started</a></li>
-              <li class="active"><a href="index.html">Documentation</a></li>
-              <!-- <li class="disabled"><a href="#">Link</a></li> -->
-              <li class="dropdown">
-                <a data-toggle="dropdown" class="dropdown-toggle" href="#">Silverbux <b class="caret"></b></a>
-                <ul role="menu" class="dropdown-menu">
-                  <li class="dropdown-header">Setting</li>
-                  <li><a href="#">Action</a></li>
-                  <li class="divider"></li>
-                  <li class="active"><a href="#">Separated link</a></li>
-                  <li class="divider"></li>
-                  <li class="disabled"><a href="#">Signout</a></li>
-                </ul>
-              </li>
-            </ul>
-
-          </div><!-- /.navbar-collapse -->
-        </div><!-- /.container-fluid -->
-      </nav>
-              <!--   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <a class="navbar-brand" href="#">Navbar</a>
-
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-              <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-                <li class="nav-item active">
-                  <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link disabled" href="#">Disabled</a>
-                </li>
-              </ul>
-              <form class="form-inline my-2 my-lg-0">
-                <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-              </form>
-            </div>
-          </nav> -->
+      
+              
     <!--header-->
     <div class="container-fluid">
     <!--documents-->
