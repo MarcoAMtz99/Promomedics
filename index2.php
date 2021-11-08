e3fa8c2019e3bfbe8504952d0d6019fc37d8c7fb
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-11-08 00:09:16 -0600

    Cambios en calendario 3

--- index2.php
+++ index2.php
@@ -184,7 +184,7 @@
 
 
                   } //Fin del if para validar isset 
-                    echo' <li id="mnu-cal"><a href="'.URL_ROOT.'/calendario.php"><i class="list-group-item"></i> Agenda</a></li>';
+                    echo' <li class="list-group-item"><a href="'.URL_ROOT.'/calendario.php"><i class="glyphicon glyphicon-align-justify"></i> Agenda</a></li>';
                     echo '<li class="list-group-item"><i class="glyphicon glyphicon-align-justify"></i> <b>SIDE PANEL</b></li>';
                   ?>
 
