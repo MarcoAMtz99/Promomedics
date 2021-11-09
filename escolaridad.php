06aa5766635a4f634fe6ae666821361470891820
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-11-09 03:15:11 -0600

    Cambios en rutas

--- escolaridad.php
+++ escolaridad.php
@@ -33,7 +33,7 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
         <!-- page content -->
-        <div class="container" >
+        <div class="container-fluid" >
 
           <div class="">
 
@@ -52,10 +52,10 @@
                     </ul>
                     <div class="clearfix"></div>
                   </div>
-                  <div class="x_content">
+                  <div class="">
 
 
-                    <div class="col-12">
+                    <div class="row">
                       <div class="col-md-12 col-sm-12 col-xs-12">
 
                         <table id="tbl-items" class="table table-striped table-bordered">
