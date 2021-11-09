9984124336e0e5bb3c89c24cfed752e702445d72
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-11-09 05:06:18 -0600

    Cambios en rutas

--- calendario.php
+++ calendario.php
@@ -638,10 +638,10 @@
 			                }else{
 			                	 $("#agendarModal").removeClass('selected');
 			                   console.log('Success');
-			                    // window.location = "https://promomedics.byw-si.com.mx/calendario.php";
+			                    window.location = "https://promomedics.byw-si.com.mx/calendario.php";
 			                }
 			            }, 'json');
-			            // location.reload();
+			            location.reload();
 
 				}
 				function actualizarCita(){
@@ -696,10 +696,10 @@
 			                }else{
 			                	 $("#agendarModal").removeClass('selected');
 			                   console.log('Success');
-			                    // window.location = "https://promomedics.byw-si.com.mx/calendario.php";
+			                    window.location = "https://promomedics.byw-si.com.mx/calendario.php";
 			                }
 			            }, 'json');
-			            // location.reload();
+			            location.reload();
 
 				}
 
