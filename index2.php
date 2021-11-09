b62b6b5743e74985cd242f788fccf47d89e66160
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-11-08 19:20:49 -0600

    Cambios en rutas

--- index2.php
+++ index2.php
@@ -215,7 +215,7 @@
 
           </div>
 
-          <div class="container-fluid content">
+          <div class="container-fluid">
               <div class="containerpanel panel-default">
                 <div class="panel-heading">
                 <h3 class="panel-title"><a href="javascript:void(0);" class="toggle-sidebar"><span class="fa fa-angle-double-left" data-toggle="offcanvas" title="Maximize Panel"></span></a> Panel de Control</h3>
