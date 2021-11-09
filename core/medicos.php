ba6032ae50cfa550311ba3e1e1ed62a1fdd7f74f
MarcoAMtz99 <marcoamtz99@gmail.com>
2021-11-09 04:29:55 -0600

    Cambios en rutas

--- core/medicos.php
+++ core/medicos.php
@@ -44,8 +44,8 @@ if($usertype > 2){
 	if($perm['action'] == 'EDIT') $edita = true;
 	
 }*/
-	if(!isset($_GET['action'])) $action = $_GET['action'];
 	
+	// $action = $_GET['action'];
 	if(!isset($_GET['action'])) $action = $_POST['action'];
 
 	if (isset($_POST['data'])) {
